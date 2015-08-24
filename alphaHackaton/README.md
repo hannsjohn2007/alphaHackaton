4e7140a5f8469731217732d89dbaa5b9348e68a6
obiniana <jrobiniana@gmail.com>
2015-08-24 15:11:57 +0800

    Included Express

---
+++ alphaHackaton/README.md
@@ -0,0 +1,3 @@
+#alphaHackaton
+
+alphaHackaton
---
+++ alphaHackaton/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+// view engine setup
+app.engine('html', require('ejs').renderFile);
+app.set('main', __dirname + '/main');
+app.set('main engine', 'html');
+
+// path setup
+app.use(express.static(path.join(__dirname, 'main')));
+
+
+// Start server
+var serverPort = 4000;
+app.listen(serverPort, function() {
+   console.log("Server started on port "+serverPort);
+});
+
+
+// Expose app
+module.exports = app;
