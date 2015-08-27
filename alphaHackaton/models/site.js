ab2f392f38ee030331e195d3724e4c0e937f3bf7
unknown <drivera@CNU1311TV6.nsn-intra.net>
2015-08-27 11:19:48 +0800

    added by Diane Rivera

--- alphaHackaton/models/site.js
+++ alphaHackaton/models/site.js
@@ -1,7 +1,7 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
-mongoose.connect('mongodb://10.127.164.149/hackathonDB');
+mongoose.connect('mongodb://10.127.165.70/hackathonDB');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
